c299d4e5893a45aa771e1583bbd2f70045f4f933
Wilson Cristian <wilson.cristvao@gmail.com>
2025-08-18 19:42:32 -0300

    alterando js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
    const body = document.querySelector("body")
+   let number = 0
 function contagem () {
 
     setTimeout(function() {
         body.style.backgroundImage = "url('assets/azul.jpg')"
     }, 1000)
 
-tempo = setInterval (function(){
-    const numero = document.querySelector("h1").innerHTML
-    const soma = parseInt(numero) + 1
-    document.querySelector("h1").innerHTML = soma
+    tempo = setInterval (function(){
+     number = number + 1; // altera a let e soma ela
+     document.querySelector("h1").innerHTML = number; // troca o conteúdo do <h1> pelo valor da variável number.
 
  }, 1000)
 
@@ -17,5 +17,6 @@ tempo = setInterval (function(){
 
 function paraContagem () {
     clearInterval(tempo)
+    
 
 }
